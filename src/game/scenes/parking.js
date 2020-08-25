b593bc11a93ced9424315da452a02722c985d77e
Tom Butterwith <thomas.butterwith@skyscanner.net>
2020-08-25 14:21:04 +0100

    adds a sweet top down car

--- src/game/scenes/parking.js
+++ src/game/scenes/parking.js
@@ -2,6 +2,9 @@ import Phaser from "phaser";
 import colours from '../../shared/assets/colours'
 
 import caravanSprite from '../../game/assets/caravan_above.png'
+import carSprite from '../../game/assets/car_above.png'
+import bushSprite from '../../game/assets/bush.png';
+import CarvaningGame from "..";
 
 const calculateVelocity = (angle, distance) => {
   const radians = (angle + 90) * (Math.PI/180);
@@ -18,13 +21,25 @@ export default class Parking extends Phaser.Scene {
 	 */
   static Name = "CaravanParking";
   
-  caravanState = {
-    rotation: 0,
-    speed: 5,
+  static carConfig = {
+    speed: 3,
+  };
+
+  static caravanConfig = {
+    speed: 1,
+  }
+
+  activeVehicle = {
+    attached: true,
+    name: '',
+    vehicle: null,
+    config: null,
   }
 
 	preload() {
 		this.load.image('caravan', caravanSprite);
+		this.load.image('car', carSprite);
+		this.load.image('bush', bushSprite);
 	}
 
 	create() {
@@ -33,54 +48,101 @@ export default class Parking extends Phaser.Scene {
     
     this.cursors = this.input.keyboard.createCursorKeys();
     this.matter.world.setBounds(0, 0, 800, 600);
+
+    this.bindKeyPresses();
   }
   
   drawScene() {
+    // SCENERY
     var background = new Phaser.Geom.Rectangle(0,0, 800, 600);
-    var leftTrack = new Phaser.Geom.Rectangle(0, 145, 800, 15);
-    var rightTrack = new Phaser.Geom.Rectangle(0, 170, 800, 15);
+    var leftTrack = new Phaser.Geom.Rectangle(0, 140, 800, 20);
+    var rightTrack = new Phaser.Geom.Rectangle(0, 170, 800, 20);
+
     var lightGreen = this.add.graphics({ fillStyle: { color: colours.LIGHT_GREEN } });
     var grey = this.add.graphics({ fillStyle: { color: colours.GREY }})
     lightGreen.fillRectShape(background);
     grey.fillRectShape(leftTrack).fillRectShape(rightTrack);
 
-    this.caravan = this.matter.add.image(400, 150, "caravan");
-    this.caravan.setFixedRotation()
-    // this.caravan.setCollideWorldBounds(true);
+    var bush = this.matter.add.image(763, 450, "bush", null, { isStatic: true });
+    bush.setScale(1.5, 7)
+    var bush = this.matter.add.image(400, 575, "bush", null, { isStatic: true });
+    bush.setScale(16, 1)
+
+    // VEHICLES
+    this.caravan = this.matter.add.image(500, 150, "caravan", null, { 
+      chamfer: { radius: [30, 30, 0, 0] },
+      frictionAir: 0.2,
+    });
+    this.caravan.setFixedRotation();
+    this.caravan.setAngle(-90);
+
+    this.car = this.matter.add.image(400, 150, "car", null, {
+      chamfer: { radius: 10 },
+      frictionAir: 0.2,
+    });
+    this.car.setAngle(-90);
+
+    this.activeVehicle = {
+      name: 'car',
+      vehicle: this.car,
+      config: Parking.carConfig,
+    };
   }
 
   update() {
-    this.caravan.setVelocity(0);
+    const { vehicle, config } = this.activeVehicle;
+
+    vehicle.setVelocity(0);
 
     if (this.cursors.left.isDown)
     {
-      this.caravanState.rotation = this.caravanState.rotation - 1;
-      this.caravan.setAngle(this.caravanState.rotation);
+      vehicle.setAngle(vehicle.angle - 1);
     }
     else if (this.cursors.right.isDown)
     {
-      this.caravanState.rotation = this.caravanState.rotation + 1;
-      this.caravan.setAngle(this.caravanState.rotation);
+      vehicle.setAngle(vehicle.angle + 1);
     }
-    else if (this.cursors.up.isDown)
+    
+    if (this.cursors.up.isDown)
     {
-      const angle = this.caravanState.rotation;
-      const speed = -this.caravanState.speed;
+      const speed = -config.speed;
 
-      const { x, y } = calculateVelocity(angle, speed);
-      this.caravan.setVelocityY(y);
-      this.caravan.setVelocityX(x);
+      const { x, y } = calculateVelocity(vehicle.angle, speed);
+      vehicle.setVelocityY(y);
+      vehicle.setVelocityX(x);
     }
     else if (this.cursors.down.isDown)
     {
-      const angle = this.caravanState.rotation;
-      const speed = this.caravanState.speed;
+      const speed = config.speed;
 
-      const { x, y } = calculateVelocity(angle, speed);
-      this.caravan.setVelocityY(y);
-      this.caravan.setVelocityX(x);
+      const { x, y } = calculateVelocity(vehicle.angle, speed);
+      vehicle.setVelocityY(y);
+      vehicle.setVelocityX(x);
     }
   }
 
+  bindKeyPresses() {
+    var spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.C);
+
+    var self = this;
+
+    spaceKey.on('up', function (key, event) {
+      event.stopPropagation();
+      if(self.activeVehicle.name === 'car') {
+        self.activeVehicle = {
+          name: 'caravan',
+          vehicle: self.caravan,
+          config: Parking.caravanConfig,
+        }
+      } else {
+        self.activeVehicle = {
+          name: 'car',
+          vehicle: self.car,
+          config: Parking.carConfig,
+        };
+      }
+
+    });
+  }
   
 }
